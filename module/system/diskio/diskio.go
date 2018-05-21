c70546fd560b6cbb47ea99c84a387952a64fcd49
Balko, Daniel <daniel.balko@wuerth-it.com>
2018-05-21 19:54:04 +0200

    Various code improvements
    Assume pointer to first element of slice is equivalent to the pointer of the underlying array.
    This removes the need for an obsucre statement with reflect and multiple unsafe.Pointer.

--- module/system/diskio/diskio.go
+++ module/system/diskio/diskio.go
@@ -7,7 +7,6 @@ package diskio
 import "C"
 
 import (
-	"reflect"
 	"unsafe"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/common/cfgwarn"
@@ -30,11 +29,9 @@ type MetricSet struct {
 	mb.BaseMetricSet
 	stats []C.perfstat_disk_t
 	first *C.perfstat_id_t
-	buffer *C.perfstat_disk_t
-	num C.int
 	sc_clk_tck uint64
-	xint uint64
-	xfrac uint64
+	sc_xint uint64
+	sc_xfrac uint64
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
@@ -53,18 +50,13 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 	num :=  C.perfstat_disk(nil, nil, C.sizeof_perfstat_disk_t, 0)
 	stats := make([]C.perfstat_disk_t, num, num)
 
-	// get a pointer to the array backing the newly created slice
-	buffer := (*C.perfstat_disk_t)(unsafe.Pointer(((*reflect.SliceHeader)(unsafe.Pointer(&stats))).Data))
-
 	return &MetricSet{
 		BaseMetricSet: base,
 		stats: stats,
 		first: first,
-		buffer: buffer,
-		num: num,
 		sc_clk_tck: uint64(C.sysconf(C._SC_CLK_TCK)),
-		xint: uint64(C._system_configuration.Xint),
-		xfrac: uint64(C._system_configuration.Xfrac),
+		sc_xint: uint64(C._system_configuration.Xint),
+		sc_xfrac: uint64(C._system_configuration.Xfrac),
 	}, nil
 }
 
@@ -72,7 +64,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 // format. It publishes the event which is then forwarded to the output. In case
 // of an error set the Error field of mb.Event or simply call report.Error().
 func (m *MetricSet) Fetch() ([]common.MapStr, error) {
-	C.perfstat_disk(m.first, m.buffer, C.sizeof_perfstat_disk_t, m.num)
+	C.perfstat_disk(m.first, &m.stats[0], C.sizeof_perfstat_disk_t, (C.int)(len(m.stats)))
 
 	events := make([]common.MapStr, 0, len(m.stats))
 	for _, counters := range m.stats {
@@ -83,12 +75,12 @@ func (m *MetricSet) Fetch() ([]common.MapStr, error) {
 			"read": common.MapStr{
 				"count": counters.xrate,
 				"bytes": counters.rblks * counters.bsize,
-				"time": uint64(counters.rserv) * m.xint / m.xfrac / 1e+6,
+				"time": uint64(counters.rserv) * m.sc_xint / m.sc_xfrac / 1e+6,
 			},
 			"write": common.MapStr{
 				"count": counters.xfers - counters.xrate,
 				"bytes": counters.wblks * counters.bsize,
-				"time": uint64(counters.wserv) * m.xint / m.xfrac / 1e+6,
+				"time": uint64(counters.wserv) * m.sc_xint / m.sc_xfrac / 1e+6,
 			},
 			"io": common.MapStr{
 				"time": uint64(counters.time) * 1000 / m.sc_clk_tck,
