388dc49bd0b785d4934f049477a02991ce8e2098
Balko, Daniel <daniel.balko@wuerth-it.com>
2018-08-20 22:10:31 +0200

    Improved ODM error handling
    care about thread safe access to odmerrno
    
    unrolled loop for clearer error handling

--- helper/odm/odm.go
+++ helper/odm/odm.go
@@ -2,6 +2,7 @@ package odm
 
 /*
 #cgo LDFLAGS: -lodm -lcfg
+#define _THREAD_SAFE_ERRNO
 #include <odmi.h>
 #include <sys/cfgodm.h>
 */
@@ -9,6 +10,7 @@ import "C"
 
 import (
 	"regexp"
+	"runtime"
 	"strings"
 	"unsafe"
 )
@@ -27,6 +29,8 @@ func (e odmError) Error() string {
 
 func Get_attribute_map(attribute string) (map[string]string, error) {
 	m := make(map[string]string)
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
 	if C.odm_initialize() == 0 {
 		defer C.odm_terminate()
 		class := C.odm_open_class_rdonly((C.CLASS_SYMBOL)(unsafe.Pointer(&C.CuAt_CLASS)))
@@ -35,14 +39,13 @@ func Get_attribute_map(attribute string) (map[string]string, error) {
 			var obj C.struct_CuAt
 			obj_ptr := unsafe.Pointer(&obj)
 			query := C.CString("attribute = " + attribute)
-			for res := C.odm_get_first(class, query, obj_ptr); res != nil; res = C.odm_get_next(class, obj_ptr) {
-				if ^uintptr(res) != 0 {
+			for res := C.odm_get_first(class, query, obj_ptr); ^uintptr(res) != 0; res = C.odm_get_next(class, obj_ptr) {
+				if res != nil {
 					m[C.GoString(&obj.name[0])] = C.GoString(&obj.value[0])
 				} else {
-					return m, odmError{C.odmerrno}
+					return m, nil
 				}
 			}
-			return m, nil
 		}
 	}
 	return m, odmError{C.odmerrno}
