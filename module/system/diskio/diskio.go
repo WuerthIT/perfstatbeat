0face1c4f1273461d41c514ed4963e5a4691b9e3
Balko, Daniel <daniel.balko@wuerth-it.com>
2019-01-10 23:35:49 +0100

    Calculate derivative values
    This has just been added as a workarround and/or comparison.
    
    Metricbeat seems to stick to incremental values so it prabably
    makes sense to comply with that (elastic/beats#2783).

--- module/system/diskio/diskio.go
+++ module/system/diskio/diskio.go
@@ -31,13 +31,23 @@ func init() {
 // interface methods except for Fetch.
 type MetricSet struct {
 	mb.BaseMetricSet
-	stats      []C.perfstat_disk_t
-	first      *C.perfstat_id_t
-	sc_clk_tck uint64
-	sc_xint    uint64
-	sc_xfrac   uint64
-	udid_map   map[string]string
-	logger     *logp.Logger
+	stats         []C.perfstat_disk_t
+	first         *C.perfstat_id_t
+	sc_clk_tck    uint64
+	sc_xint       uint64
+	sc_xfrac      uint64
+	udid_map      map[string]string
+	logger        *logp.Logger
+	last_counters map[string]DiskCounters
+}
+
+type DiskCounters struct {
+	read_count  uint64
+	read_bytes  uint64
+	read_time   uint64
+	write_count uint64
+	write_bytes uint64
+	write_time  uint64
 }
 
 // New creates a new instance of the MetricSet. New is responsible for unpacking
@@ -57,6 +67,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	num := C.perfstat_disk(nil, nil, C.sizeof_perfstat_disk_t, 0)
 	stats := make([]C.perfstat_disk_t, num, num)
+	last_counters := make(map[string]DiskCounters)
 
 	udid_map, err := odm.Get_attribute_map("unique_id")
 	if err != nil {
@@ -72,6 +83,7 @@ func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 		sc_xfrac:      uint64(C._system_configuration.Xfrac),
 		udid_map:      udid_map,
 		logger:        logger,
+		last_counters: last_counters,
 	}, nil
 }
 
@@ -85,24 +97,50 @@ func (m *MetricSet) Fetch() ([]common.MapStr, error) {
 	for _, counters := range m.stats {
 
 		name := C.GoString(&counters.name[0])
+
+		current := DiskCounters{
+			read_count:  uint64(counters.xrate),
+			read_bytes:  uint64(counters.rblks * counters.bsize),
+			read_time:   uint64(counters.rserv) * m.sc_xint / m.sc_xfrac / 1e+6,
+			write_count: uint64(counters.xfers - counters.xrate),
+			write_bytes: uint64(counters.wblks * counters.bsize),
+			write_time:  uint64(counters.wserv) * m.sc_xint / m.sc_xfrac / 1e+6,
+		}
+
 		event := common.MapStr{
 			"name":   name,
 			"vgname": C.GoString(&counters.vgname[0]),
 			"udid":   m.udid_map[name],
 			"read": common.MapStr{
-				"count": counters.xrate,
-				"bytes": counters.rblks * counters.bsize,
-				"time":  uint64(counters.rserv) * m.sc_xint / m.sc_xfrac / 1e+6,
+				"count": current.read_count,
+				"bytes": current.read_bytes,
+				"time":  current.read_time,
 			},
 			"write": common.MapStr{
-				"count": counters.xfers - counters.xrate,
-				"bytes": counters.wblks * counters.bsize,
-				"time":  uint64(counters.wserv) * m.sc_xint / m.sc_xfrac / 1e+6,
+				"count": current.write_count,
+				"bytes": current.write_bytes,
+				"time":  current.write_time,
 			},
 			"io": common.MapStr{
 				"time": uint64(counters.time) * 1000 / m.sc_clk_tck,
 			},
 		}
+
+		if last, ok := m.last_counters[name]; ok {
+			event["derivative"] = common.MapStr{
+				"read": common.MapStr{
+					"count": current.read_count - last.read_count,
+					"bytes": current.read_bytes - last.read_bytes,
+					"time":  current.read_time - last.read_time,
+				},
+				"write": common.MapStr{
+					"count": current.write_count - last.write_count,
+					"bytes": current.write_bytes - last.write_bytes,
+					"time":  current.write_time - last.write_time,
+				},
+			}
+		}
+		m.last_counters[name] = current
 
 		events = append(events, event)
 	}
